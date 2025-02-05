19e7876af686ea235a94aed8eead8f7bff217d91
Lilian <liliansod22@gmail.com>
2025-02-04 22:10:48 -0300

    Lilian

--- controllers/carroControllers.js
+++ controllers/carroControllers.js
@@ -1,5 +1,5 @@
 import { getAllcarros, getCarroBySigla, createCarro as modelCreateCarro, updateCarro as modelCreateCarro, deleteCarro as modelDeleteCarro} from '.../models/carroModel.js'
-import { getAllUsuarios } from '../models/usuarioModels';
+import { getAllUsuarios, getUsuarioByEmail } from '../models/usuarioModels';
 import { modeloAtualizaçãoUsuario } from '../Validations/usuarioValidation';
 
 export const getCarros =(req, res) => {
@@ -83,73 +83,4 @@ export const deleteCarro =(req, res) => {
     res
         .status(200)
         .json({mensagem: "Carro removido com sucesso!", carro: carroRemovido});
-};
-
-// npm install jsonwebtoken / npm install bcrypt
-
-// chave secreta para assinatura do token JWT
-const JWT_SECRET = 'secreta-chave';
-
-// Função para obter todos os usuários
-export const getUsuarios = (req, res) =>{
-    // Busca usuarios "Em memoria, deveria ser banco de dados"
-    const usuarios = getAllUsuarios();
-    // Retorna os usuários com status 200
-    res.status(200).json(usuarios);
-};
-
-// Fução para obter um unico usuário pelo ID
-export const getUsuario = (req, res) =>{
-    const {id} = req.params;
-    const usuario = usuario.find((user) => user.id === parseInt(id));
-    
-    if(!usuario){
-        return res.status(404).json({mensagem: 'Usuário não encontrado!'});
-    }
-
-    res.status(200).json(usuario);
-};
-
-// Função para criar usuário
-export const createUsuario = (req, res) => {
-    const { erro } = modeloUsuario.validate(req.body);
-
-    if(erro){
-        return res.status(400).json({mensagem: error.details[0].message});
-    }
-
-    const novoUsuario = req.body;
-    const usuarioCriado = createUsuario(usuario);
-
-    res.status(201).json(usuarioCriado);
-};
-
-// Função para atualizar função de usuario
-export const updateUsuario = (req, res) => {
-    const { id } = req.params;
-    const { error } = modeloAtualizaçãoUsuario.validate(req.body);
-
-    if (error) {
-        return res.status(400).json({mensagem: error.details[0].message});
-    }
-
-    const usuarioAtualizado = updateUsuario(id, req.body);
-
-    if (!usuarioAtualizado) {
-        return res.status(404).json({mensagem: 'Usuario não encontrado para atualização!'});
-    }
-
-    res.status(200).json(usuarioAtualizado);
-};
-
-//Função para excluir um usuário
-export const deleteUsuario =(req, res) => {
-    const { id } = req.params;
-    const usuarioRemovido = deleteUsuario(id);
-
-    if (!usuarioRemovido) {
-        return res.status(404).json({mensagem: 'Usuario não encontrado para remoção!'});
-    }
-
-    res.status(200).json({mensagem: 'Usuario removido com sucesso!', usuario: usuarioRemovido});
 };
